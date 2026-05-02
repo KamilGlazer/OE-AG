04583576472c1bf513c6df2d49f08607a1a6af46
Yuviellia <56468073+Yuviellia@users.noreply.github.com>
2026-05-02 11:55:17 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,13 +1,4 @@
-import math
-
 def fitness_martin_gaddy(x):
-    """
-    Martin and Gaddy test function.
-    Global minimum: x_i = 5 for all i, f(x) = 0
-    Function extended to N-dimensions by summing over adjacent pairs.
-    f(x) = sum_{i=0}^{N-2} [ (x_i - x_{i+1})^2 + ((x_i + x_{i+1} - 10)/3)^2 ]
-    If N=1, we can just return ((x[0] - 5)/3)**2 
-    """
     n = len(x)
     if n == 1:
         return ((x[0] - 5)/3)**2
@@ -19,7 +10,6 @@ def fitness_martin_gaddy(x):
         val += term1 + term2
     return val
 
-# A dictionary to easily add more functions to the GUI
 AVAILABLE_FUNCTIONS = {
     "Martin and Gaddy": fitness_martin_gaddy
 }
